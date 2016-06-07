1322f6b97b548303dc6d3937ac714554ec8c3afe
MarkPThomas <markums@gmail.com>
2016-06-06 22:17:39 -0700

    First Commit

---
+++ Ch 2 - A First Unit Test/LogAn/LogAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool WasLastFileNameValid { get; set; }
+
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("filename has to be provided");
+            }
+
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            WasLastFileNameValid = true;
+            return true;
+        }
+
+    }
+}
---
+++ Ch 2 - A First Unit Test/LogAn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    class Program
+    {
+        //ncrunch: no coverage start
+        static void Main(string[] args)
+        {
+            LogAnalyzer logAn = new LogAnalyzer();
+            logAn.IsValidLogFileName("ValidFileName.SLF");
+        }
+        //ncrunch: no coverage end
+    }
+}
